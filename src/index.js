70e0916a3db598ffe2b18d6c7272ad8b8e00540c
Sarbeh <handoyonoterakhir@gmail.com>
2023-06-24 16:13:59 +0700

    update prefilight

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-
-
 export async function corsMiddleware({ request }, next) { 
-  const response = await next();
-  const headers = new Headers(response.headers);
+  if (request.method === 'OPTIONS') {
+    let headers = new Headers();
+    headers.append('Access-Control-Allow-Origin', '*');
+    headers.append('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+    headers.append('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    return new Response(null, { headers });
+  }
 
+  const response = await next();
+
+  const headers = new Headers(response.headers);
   headers.append('Access-Control-Allow-Origin', '*');
   headers.append('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
   headers.append('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-
+  
   return new Response(response.body, { 
     ...response, 
     headers: headers, 
